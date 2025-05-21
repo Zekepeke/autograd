182ce9d0efe8eb2317627273e987755f37265ec9
Esequiel Linares <esequiellinares06@gmail.com>
2025-05-20 21:45:30 -0400

    Build the main part of the engine in python

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 #Autograd
 A class of mathical expression
 scalar valued auto grad engine 
+
+
+children is a tuple for convenience but maintaining it into a set, used for keeping track of pointers for what values produce what other values previously
+
+
+
+operation is used for keeping track of the mathematical operation that created the new value of the children as we know the value of every single child but not the operation used to create a new value
+
+backward function is the thing that does the piece of chain rule at each node that takes inputs and produces an output 
--- autograd.py
+++ autograd.py
@@ -1,13 +1,75 @@
+import math
+
 class Node:
-    def __init__(self, data):
+    def __init__(self, data, children=(), operation='', label=''):
         self.data = data
-    def __repr__(self):
-        return f"Value(data={self.data})"
+        self.grad = 0.0
+        self.backward = lambda: None
+        self.prev = set(children)
+        self.op = operation
+        self.label = label
+        
     def __add__(self, other):
-        return Node(self.data + other.data)
+        out = Node(self.data + other.data, (self, other), '+')
+    
+        def back_prop():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        # Storing the function in the backward
+        out.backward = back_prop
+    
+        return out
     def __mul__(self, other):
-        return Node(self.data * other.data)
+        other = other if isinstance(other, Node) else Node(other)
+        out = Node(self.data * other.data, (self, other), '*')
+
+        def back_prop():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        # Storing the function in the backward
+        out.backward = back_prop
+
+        return out
     def __pow__(self, other):
-        return Node(self.data ** other.data)
+        return Node(self.data ** other.data, (self, other), '**')
     def __truediv__(self, other):
-        return Node(self.data / other.data)
+        return Node(self.data / other.data, (self, other), '/')
+    
+    def tanh(self):
+        x = self.data
+        # Definition of tanh
+        tan = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+        out = Node(tan, (self, ), 'tanh')
+        
+        def back():
+            self.grad += (1 - tan**2) * out.grad
+        # Storing the function in the backward
+        out.backward = back
+        
+        return out
+
+    def backward(self):
+        # Topological sort         
+        top = []
+        seen = set()
+        
+        # Recursively build the topological sort
+        def build_topo(v):
+            if v not in seen:
+                seen.add(v)
+                for child in v.prev:
+                    build_topo(child)
+                top.append(v)
+        build_topo(self)
+        self.grad = 1.0
+        
+        # Starting from the root node and going backwards
+        for node in reversed(top):
+            node.backward()
+        
+
+    
+    # For pritning the node value, the children, and the operation
+    def __repr__(self):
+        return f"Node(data={self.data})"
+    
